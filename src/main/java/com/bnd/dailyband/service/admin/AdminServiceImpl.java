7af86ad4d2a569b5de8d72c442a28a808c0f79e7
Doyeon kim <doyeon797@gmail.com>
2024-05-30 17:20:41 +0900

    feat: 관리자페이지 회원관리 기능 구현 관리자만 회원 권한 변경 기능 회원 상태 변경 기능 Ref #7

--- src/main/java/com/bnd/dailyband/service/admin/AdminServiceImpl.java
+++ src/main/java/com/bnd/dailyband/service/admin/AdminServiceImpl.java
@@ -19,32 +19,35 @@ public class AdminServiceImpl implements AdminService{
   }
 
   @Override
-  public int getSearchListCount(int index, String searchWord) {
-    Map<String, String> map = new HashMap<>();
-    if(index != -1) {
-      String[] search_field = new String[] {"id", "name", "age", "gender"};
-      map.put("search_field", search_field[index]);
-      map.put("search_word", "%" + searchWord + "%");
-    }
-    return adao.getSearchListCount(map);
+  public List<Member> getMemberList() {
+    Map<String, Object> map = new HashMap<>();
+    return adao.getMemberList(map);
   }
 
   @Override
-  public List<Member> getSearchList(int index, String searchWord, int page, int limit) {
-    Map<String, Object> map = new HashMap<>();
-
-    //http://localhost:9000/dailyband/admin/mbrmgmt 로 접속하는 경우
-    //select를 선택하지 않아 index는 "-1"의 값을 갖습니다.
-    //이 경우 아래의 문장을 수행하지 않기 때문에 "search_field" 키에 대한 map.get("search_field")의 값은 null
-    if(index != -1) {
-      String[] search_field = new String[] {"id", "name", "age", "gender"};
-      map.put("search_field", search_field[index]);
-      map.put("search_word", "%" + searchWord + "%");
-    }
-    int startrow = (page-1) * limit + 1;
-    int endrow = startrow + limit - 1;
-    map.put("start", startrow);
-    map.put("end", endrow);
-    return adao.getSearchList(map);
+  public void memberTY(String id) {
+    adao.memberTY(id);
   }
+
+  @Override
+  public void managerTY(String id) {
+    adao.managerTY(id);
+  }
+
+  @Override
+  public void adminTY(String id) {
+    adao.adminTY(id);
+  }
+
+  @Override
+  public void memberStatusAc(String id) {
+    adao.memberStatusAc(id);
+  }
+
+  @Override
+  public void memberStatusIn(String id) {
+    adao.memberStatusIn(id);
+  }
+
+
 }
